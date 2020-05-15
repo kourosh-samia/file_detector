2cc2b78caf8bd45c9a110bfcc12b39d959f33931
Kourosh Samia <kourosh-samia@users.noreply.github.com>
2020-05-15 15:44:36 -0400

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+//error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', 1);
+ini_set('memory_limit', '256M');
+require_once 'lib/FileHandler.php';
+require_once 'Command.php';
+require_once 'Functions.php';
+$info = [];
+    
+$command = new Command($argv);
+
+// Load the Help manual on the console
+if ($command->getHelp()) { die(file_get_contents('help.txt').PHP_EOL);}
+
+
+$info = [
+        'content-path'         => $command->getContentPath(),
+        'dryrun'               => $command->getDryRun(),
+        'verbose'              => $command->getVerbose(),
+        'progress'             => $command->getProgress()
+        ];
+    
